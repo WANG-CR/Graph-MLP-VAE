bc373c011d98aac3a13c1d81ccf253c11426b49d
ChildishBob <weifutao2019@gmail.com>
2021-07-06 16:28:55 +0800

    fix Ncontrast

--- Graph-MLP-master/train.py
+++ Graph-MLP-master/train.py
@@ -120,8 +120,10 @@ def Ncontrast(x_dis, adj_label, tau = 1):
     x_dis = torch.exp(tau * x_dis)
     x_dis_sum = torch.sum(x_dis, 1)
     x_dis_sum_pos = torch.sum(x_dis*adj_label, 1)
-    loss = -torch.log((x_dis_sum_pos+1e-8) * (x_dis_sum**(-1)+1e-8)).mean()
+    #loss = -torch.log((x_dis_sum_pos+1e-8) * (x_dis_sum**(-1)+1e-8)).mean()
     #reasonable or not? smoothing both denominator and numerator
+    loss = -torch.log(x_dis_sum_pos * (x_dis_sum**(-1))+1e-8).mean()
+    
     return loss
 
 def Ncontrast2(xy_dis, tau = 1):
